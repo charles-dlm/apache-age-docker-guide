39f5ca4e1ebda320af73e392f51cd430143ed078
Charles Del <117399064+charles-dlm@users.noreply.github.com>
2026-06-02 11:58:05 +0200

    Corrected some minor errors on init.sql

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
--- 1. Initialiser l'extension AGE
+-- 1. Initialize the AGE extension
 CREATE EXTENSION IF NOT EXISTS age CASCADE;
 
--- 2. Charger AGE dans le path de cette session
+-- 2. Load AGE into the session search path
 LOAD 'age';
 SET search_path = ag_catalog, "$user", public;
 
--- 3. Créer un graphe nommé "reseau_social"
+-- 3. Create a graph named "social_media"
 SELECT create_graph('social_media');
 
--- 4. Insérer des données de test (Syntaxe Cypher)
-SELECT * FROM cypher('reseau_social', $$
+-- 4. Insert test data (Cypher syntax)
+SELECT * FROM cypher('social_media', $$
     CREATE (a:Person {name: 'Alice', age: 30})
     CREATE (b:Person {name: 'Bob', age: 25})
     CREATE (a)-[r:KNOWS {since: 2021}]->(b)
